38c094624b2af7b8e70c5471dccc5b04c5c69c3b
vishy456 <vishy456@yahoo.com>
2014-04-28 09:05:26 +0800

    Update CacheMatrix.R
    Updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
 ## To demonstrate ability to cache results of data intensive operations
 
-## makeCacheMatrix provides a list of interfaces to store value of matrix and its computed result
+## makeCacheMatrix() function provides a list of interfaces - 
+## set() - To store the contents of a matrix, to be retrieved across different environment
+## get() - To retrieve the contents of matrix
+## setInverse() - To store the Inverse matrix, to be retrieved across different environment
+## getInverse() - To retrieve the inverse matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
   InvMatrix <- NULL
@@ -16,17 +20,17 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-
-## cacheSolve function tries to retrieve the already stored inverse matrix, if any. If not, computes and 
-## store the result for furture reference
+## cacheSolve() function tries to retrieve the already stored inverse matrix, if any. If not, computes and 
+## store the result for future reference and return the just computed inverse
 
 cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
+  ## getInverse will return valid inverse matrix if already stored inverse matrix is available
   InvMatrix <- x$getInverse()
   if(!is.null(InvMatrix)) {
     message("getting cached data")
     return(InvMatrix)
   }
+  ## If the cachekSolve is called for first time for a matrix, it will be computed,stored and returned to caller as below
   data <- x$get()
   InvMatrix <- solve(data, ...)
   x$setInverse(InvMatrix)
